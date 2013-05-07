8e38c3ff84cdccdb17f61af19ff777267ec26769
snoooze03 <snoooze03@gmail.com>
2013-05-07 15:39:36 +0200

    Some code comments

--- src/main/java/ch/noisette/doodle/services/impl/DefaultPollService.java
+++ src/main/java/ch/noisette/doodle/services/impl/DefaultPollService.java
@@ -27,12 +27,21 @@ import ch.noisette.doodle.domains.Poll;
 import ch.noisette.doodle.domains.Subscriber;
 import ch.noisette.doodle.services.PollService;
 
+/**
+ * 
+ * @author Phokham Nonanva
+ * @author Stefan Nüesch
+ * 
+ */
 @Service
 public class DefaultPollService implements PollService {
 
 	private static final Logger log = Logger
 			.getLogger(DefaultPollService.class);
 
+	/**
+	 * Constants for field access
+	 */
 	private static final String KEYSPACE = "doodle";
 	private static final String COL_FAMILIY_POLL = "poll";
 	private static final String COL_FAMILIY_SUBSCRIBER = "subscriber";
@@ -42,18 +51,25 @@ public class DefaultPollService implements PollService {
 
 	private final Cluster cluster;
 	private final Keyspace keyspace;
+
+	/**
+	 * Hector templates for the two column families
+	 */
 	private final ColumnFamilyTemplate<String, String> pollTemplate;
 	private final ColumnFamilyTemplate<String, String> subscriberTemplate;
 
 	private final ObjectMapper mapper;
 
 	public DefaultPollService() {
+		// Used to serialize choices in JSON representation
 		this.mapper = new ObjectMapper();
 		this.mapper.configure(
 				DeserializationConfig.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY,
 				true);
+		// Cassandra connection & Keyspace
 		this.cluster = HFactory.getOrCreateCluster("clst", "localhost:9160");
 		this.keyspace = HFactory.createKeyspace(KEYSPACE, this.cluster);
+		// initialize templates
 		this.pollTemplate = new ThriftColumnFamilyTemplate<>(this.keyspace,
 				COL_FAMILIY_POLL, StringSerializer.get(),
 				StringSerializer.get());
@@ -65,9 +81,11 @@ public class DefaultPollService implements PollService {
 	@Override
 	public Poll addSubscriber(String pollId, Subscriber subscriber) {
 		ColumnFamilyUpdater<String, String> subsUpdater = null;
+		// PollID and subscriber label concatenated for subscriber id
 		String key = pollId + subscriber.getLabel();
 		subsUpdater = this.subscriberTemplate.createUpdater(key);
 		subsUpdater.setString(LABEL, subscriber.getLabel());
+		// serializing list of choices with JSON
 		try {
 			subsUpdater.setString(CHOICES,
 					this.mapper.writeValueAsString(subscriber.getChoices()));
@@ -80,21 +98,24 @@ public class DefaultPollService implements PollService {
 
 	@Override
 	public Poll createPoll(Poll poll) {
+		// If id already set, do nothing. otherwise, Hector provides a way to
+		// get unique ids (UUID using time)
 		if (poll.getId() == null || poll.getId().isEmpty()) {
 			poll.setId(TimeUUIDUtils.getUniqueTimeUUIDinMillis().toString());
 		}
 		ColumnFamilyUpdater<String, String> pollUpdater = this.pollTemplate
 				.createUpdater(poll.getId());
 		pollUpdater.setString(LABEL, poll.getLabel());
+		// serializing list of choices with JSON
 		try {
 			pollUpdater.setString(CHOICES,
 					this.mapper.writeValueAsString(poll.getChoices()));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		pollUpdater.setString(EMAIL, poll.getEmail());
 		this.pollTemplate.update(pollUpdater);
+		// Add all scubscribers
 		if (poll.getSubscribers() != null) {
 			for (Subscriber subs : poll.getSubscribers()) {
 				this.addSubscriber(poll.getId(), subs);
@@ -116,11 +137,14 @@ public class DefaultPollService implements PollService {
 	@Override
 	public List<Poll> getAllPolls() {
 		List<Poll> polls = new ArrayList<>();
+		// To get all Polls, a simple CQL query is needed
 		CqlQuery<String, String, String> query = new CqlQuery<>(this.keyspace,
 				StringSerializer.get(), StringSerializer.get(),
 				StringSerializer.get());
 		query.setQuery("SELECT * FROM " + COL_FAMILIY_POLL);
 		QueryResult<CqlRows<String, String, String>> result = query.execute();
+
+		// excract query result data and map to Poll
 		for (Row<String, String, String> row : result.get().getList()) {
 			Poll poll = new Poll();
 			poll.setId(row.getKey());
@@ -133,7 +157,6 @@ public class DefaultPollService implements PollService {
 			try {
 				poll.setChoices(this.mapper.readValue(choices, ArrayList.class));
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			poll.setSubscribers(this.getSubscribersByPollId(poll.getId()));
@@ -146,6 +169,7 @@ public class DefaultPollService implements PollService {
 	@Override
 	public Poll getPollById(String pollId) {
 		Poll poll = new Poll();
+		// hector provides an easy way to query by key
 		ColumnFamilyResult<String, String> res = this.pollTemplate
 				.queryColumns(pollId);
 		if (!res.hasResults()) {
@@ -159,15 +183,18 @@ public class DefaultPollService implements PollService {
 					ArrayList.class);
 			poll.setChoices(choices);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		// read subscribers
 		poll.setSubscribers(this.getSubscribersByPollId(pollId));
 
 		return poll;
 	}
 
+	/*
+	 * Uses CQL to query subscribers by PollID
+	 */
 	private List<Subscriber> getSubscribersByPollId(String pollId) {
 		List<Subscriber> subscribers = new ArrayList<>();
 		CqlQuery<String, String, String> query = new CqlQuery<>(this.keyspace,
@@ -179,6 +206,9 @@ public class DefaultPollService implements PollService {
 		return this.getSubscribersFromResult(result);
 	}
 
+	/*
+	 * Convenience mapper method
+	 */
 	private List<Subscriber> getSubscribersFromResult(
 			QueryResult<CqlRows<String, String, String>> result) {
 		List<Subscriber> subscribers = new ArrayList<>();
